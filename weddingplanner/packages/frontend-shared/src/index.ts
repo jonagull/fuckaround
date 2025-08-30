5ddf19bf3d3b319c3e28a209cee2307b3fa0105b
Vegard Lokreim <vegardlokreim9@gmail.com>
2025-08-30 21:50:04 +0200

    no more docker for web and backend, only db

--- weddingplanner/packages/frontend-shared/src/index.ts
+++ weddingplanner/packages/frontend-shared/src/index.ts
@@ -2,3 +2,5 @@ export * from "./api";
 export * from "./hooks";
 export { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export * from "weddingplanner-types";
